f51eb4e7b6a816ef1c5749fceef1bd0261f77551
Bruno Alves <alves.bruno945@gmail.com>
2025-07-21 23:14:08 -0300

    Add buttons that increment and decrement the value

--- JustCount/CountView.swift
+++ JustCount/CountView.swift
@@ -1,19 +1,49 @@
 import SwiftUI
 
 struct CountView: View {
+
+    @State private var number = 0
+    @State private var viewModel = CounterViewModel()
+
     var body: some View {
-        VStack {
+        VStack(spacing: 10) {
+            Text("\(viewModel.number)")
+                .font(.title)
+                .fontWeight(.bold)
+
             Button("+1") {
-                print("Count One..")
+                viewModel.incrementOne()
             }
             .foregroundStyle(.black)
             .buttonStyle(.bordered)
+
+            Button("+5") {
+                viewModel.incrementFive()
+            }
+            .foregroundStyle(.black)
+            .buttonStyle(.bordered)
+            
+            Button("-5") {
+                viewModel.decrementFive()
+            }
+            .foregroundStyle(.black)
+            .buttonStyle(.bordered)
+
+            Button("Reset") {
+                viewModel.reset()
+            }
+            .foregroundStyle(.black)
+            .buttonStyle(.bordered)
+
+            Text(viewModel.handleNumberStatus())
+                .font(.callout)
+                .fontWeight(.bold)
         }
     }
 }
 
+
+
 // MARK: - Preview
 
-#Preview {
-    CountView()
-}
+#Preview { CountView() }
