60254d23920b1d2528db56e2096907adb1494281
Bruno Alves <alves.bruno945@gmail.com>
2025-07-21 22:25:33 -0300

    Initial commit

---
+++ JustCount/JustCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustCountApp.swift
+//  JustCount
+//
+//  Created by Bruno Silva on 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct JustCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JustCountTests/JustCountTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustCountTests.swift
+//  JustCountTests
+//
+//  Created by Bruno Silva on 21/07/25.
+//
+
+import Testing
+@testable import JustCount
+
+struct JustCountTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
