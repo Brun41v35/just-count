f69fa1148e0fb2880bd6d7cc464ea497b7a7616e
Bruno Alves <alves.bruno945@gmail.com>
2025-07-22 20:37:17 -0300

    Extract `CounterViewModel` to property and add init

--- JustCountTests/CounterViewModelTests.swift
+++ JustCountTests/CounterViewModelTests.swift
@@ -3,10 +3,14 @@ import Testing
 
 struct CounterViewModelTests {
 
+    private var sut: CounterViewModel
+
+    init() {
+        sut = CounterViewModel()
+    }
+
     @Test
     func test_incrementOne_shouldIncrementOneMore() {
-        let sut = CounterViewModel()
-
         sut.incrementOne()
 
         #expect(sut.number == 1, "Expect to increment one more")
@@ -14,8 +18,6 @@ struct CounterViewModelTests {
 
     @Test
     func test_incrementFive_shouldIncrementFiveMore() {
-        let sut = CounterViewModel()
-
         sut.incrementFive()
 
         #expect(sut.number == 5, "Expect to increment five more")
@@ -23,8 +25,6 @@ struct CounterViewModelTests {
 
     @Test
     func test_decrementFive_shouldDecrementFiveMore() {
-        let sut = CounterViewModel()
-
         sut.decrementFive()
 
         #expect(sut.number == -5, "Expect to decrement five more")
@@ -32,8 +32,6 @@ struct CounterViewModelTests {
 
     @Test
     func test_reset_shouldResetTheNumber() {
-        let sut = CounterViewModel()
-
         sut.reset()
 
         #expect(sut.number == 0, "Expect to reset the count")
@@ -41,8 +39,6 @@ struct CounterViewModelTests {
 
     @Test
     func test_handleNumberStatus_shouldBePositive() {
-        let sut = CounterViewModel()
-
         let receivedValue = sut.handleNumberStatus()
 
         #expect(receivedValue == "Positive", "Expect that `receivedValue` be Positive")
@@ -50,7 +46,6 @@ struct CounterViewModelTests {
 
     @Test
     func test_handleNumberStatus_shouldBeNegative() {
-        let sut = CounterViewModel()
         sut.number = -10
 
         let receivedValue = sut.handleNumberStatus()
